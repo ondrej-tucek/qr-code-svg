e57a31119e71a972a1a2ce2e7de336f9cdd3e4ab
Ondrej Tucek <ondrej.tucek@gmail.com>
2018-05-19 15:34:27 +0200

    Bump version: 0.1.0 → 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule QrCodeSvg.MixProject do
   def project do
     [
       app: :qr_code_svg,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
