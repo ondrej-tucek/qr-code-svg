e0293d354b93e195c5eb321893b52bdd0968e037
Ondrej Tucek <ondrej.tucek@gmail.com>
2018-05-19 15:41:14 +0200

    Bump version: 1.0.0 → 1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule QrCodeSvg.MixProject do
   def project do
     [
       app: :qr_code_svg,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       desciption: "Library for generating QR code to SVG format.",
